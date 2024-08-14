b9abfe78c4f35c8920cf9ed626ca2cc99f4c6ab1
Sakari Palojoki <palojoki.sakari@gmail.com>
2024-08-14 19:12:19 +0300

    Format Layout with Prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,29 @@
-import type { Metadata } from "next";
-import { Roboto_Flex } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next"
+import { Roboto_Flex, Sacramento } from "next/font/google"
+import "./globals.css"
 
-const roboto = Roboto_Flex({ subsets: ["latin"] });
+const roboto = Roboto_Flex({ subsets: ["latin"] })
+
+const sacramento = Sacramento({
+  variable: "--signature-font",
+  weight: "400",
+  subsets: ["latin"],
+})
 
 export const metadata: Metadata = {
   title: "SPalojoki Portfolio",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className="scroll-smooth">
-      <body className={roboto.className}>{children}</body>
+    <html lang='en' className='scroll-smooth'>
+      <body className={`${roboto.className} ${sacramento.variable}`}>
+        {children}
+      </body>
     </html>
-  );
+  )
 }
